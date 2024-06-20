5cf0e899f1b70105f292a191e5047db09ebbb375
Ariel Barreiro <abarrei@gmail.com>
2024-06-20 15:25:38 -0300

    chore(ddev): composer no interaction and automate start

---
+++ .ddev/post-start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e -o pipefail
+
+./.ddev/commands/web/poser
+
+# If db empty, do initial setup
+if [ -z "$(drush status --field=bootstrap)" ]; then
+  drush si --site-name=custom-admin-toolbar --account-pass=1 -y
+  # This normally is take care by config.contrib.yaml but if this was run with
+  # freshly cloned project, poser needs to be run manually, but we are automating
+  # that
+  ./.ddev/commands/web/symlink-project
+  drush en -y devel custom_admin_toolbar
+fi
+
+# Always output this
+drush uli
